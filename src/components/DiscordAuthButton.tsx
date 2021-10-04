69445641a37bfad8d22aed683861f4164e90d252
Noah <noah.drabinsky1@gmail.com>
2021-10-03 20:11:51 -0600

    Set up staging auth url

--- src/components/DiscordAuthButton.tsx
+++ src/components/DiscordAuthButton.tsx
@@ -5,22 +5,31 @@ import { useAppSelector } from "../app/hooks";
 import { RootState } from "../app/store";
 import { DiscordUserAvatar } from "../components";
 
-// Gilder permission int: 1342180432
-
-// TODO use env variable for discord auth url
+// TODO: make this based off env vars
 const discordAuthUrl =
   "https://discord.com/api/oauth2/authorize?client_id=894327916727504946&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=guilds%20identify";
+const discordStagingUrl = `https://discord.com/api/oauth2/authorize?client_id=894327916727504946&redirect_uri=https%3A%2F%2Fgilder.vercel.app%2F&response_type=code&scope=identify%20guilds`;
+
+const testURL = `https://discord.com/api/oauth2/authorize?client_id=894327916727504946&redirect_uri=${process.env.REACT_APP_GILDER_URL}2F&response_type=code&scope=guilds%20identify`;
+
+console.log("testing env discord url", testURL);
 
 export const DiscordAuthButton: FC = () => {
   const user = useAppSelector((state: RootState) => state.discord.user);
 
-  console.log("user?", user);
   return (
     <Box sx={{ justifyContent: "center" }}>
       {user ? (
         <DiscordUserAvatar user={user} />
       ) : (
-        <Button variant="contained" href={discordAuthUrl}>
+        <Button
+          variant="contained"
+          href={
+            process.env.NODE_ENV === "development"
+              ? discordAuthUrl
+              : discordStagingUrl
+          }
+        >
           Connect Discord
         </Button>
       )}
